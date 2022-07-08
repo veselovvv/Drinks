0f70d72bddd708f77a0edb869466184b0ac9dbf8
veselovvv <vycheslav5veselov@gmail.com>
2022-07-08 10:27:15 +0300

    Fixed unit tests for presentation layer

--- app/src/test/java/com/veselovvv/drinks/presentation/cocktaildetails/BaseCocktailsDetailsDomainToUiMapperTest.kt
+++ app/src/test/java/com/veselovvv/drinks/presentation/cocktaildetails/BaseCocktailsDetailsDomainToUiMapperTest.kt
@@ -1,53 +1,43 @@
 package com.veselovvv.drinks.presentation.cocktaildetails
 
-import com.veselovvv.drinks.R
+import com.veselovvv.drinks.TestResourceProvider
 import com.veselovvv.drinks.core.ErrorType
-import com.veselovvv.drinks.core.ResourceProvider
 import com.veselovvv.drinks.domain.cocktaildetails.CocktailDetailsDomain
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class BaseCocktailsDetailsDomainToUiMapperTest {
     private val cocktailDetailsDomainToUiMapper = BaseCocktailDetailsDomainToUiMapper()
-    private val cocktailsDetailsDomainToUiMapper = BaseCocktailsDetailsDomainToUiMapper(
+    private val mapper = BaseCocktailsDetailsDomainToUiMapper(
         TestResourceProvider(), cocktailDetailsDomainToUiMapper
     )
 
     @Test
     fun test_success() {
-        val data = CocktailDetailsDomain(
+        val cocktailDetails = CocktailDetailsDomain(
             "Alcoholic",
             "Cocktail glass",
             "Rub the rim of the glass with the lime slice to make the salt stick to it.",
             listOf("Tequila", "Triple sec", "Lime juice", "Salt", "", "", "", "", "", "")
         )
-        val expected = CocktailsDetailsUi.Success(data, cocktailDetailsDomainToUiMapper)
-        val actual = cocktailsDetailsDomainToUiMapper.map(data)
+        val expected = CocktailsDetailsUi.Success(cocktailDetails, cocktailDetailsDomainToUiMapper)
+        val actual = mapper.map(cocktailDetails)
         assertEquals(expected, actual)
     }
 
     @Test
     fun test_fail() {
         var expected = CocktailsDetailsUi.Fail(NO_CONNECTION_MESSAGE)
-        var actual = cocktailsDetailsDomainToUiMapper.map(ErrorType.NO_CONNECTION)
+        var actual = mapper.map(ErrorType.NO_CONNECTION)
         assertEquals(expected, actual)
 
         expected = CocktailsDetailsUi.Fail(SERVICE_UNAVAILABLE_MESSAGE)
-        actual = cocktailsDetailsDomainToUiMapper.map(ErrorType.SERVICE_UNAVAILABLE)
+        actual = mapper.map(ErrorType.SERVICE_UNAVAILABLE)
         assertEquals(expected, actual)
-    }
-
-    class TestResourceProvider : ResourceProvider {
-        override fun getString(id: Int) = when (id) {
-            R.string.no_connection_message -> NO_CONNECTION_MESSAGE
-            R.string.service_unavailable_message -> SERVICE_UNAVAILABLE_MESSAGE
-            else -> SOMETHING_WENT_WRONG
-        }
     }
 
     companion object {
         private const val NO_CONNECTION_MESSAGE = "No connection. Please try again!"
         private const val SERVICE_UNAVAILABLE_MESSAGE = "Service unavailable. Please try again!"
-        private const val SOMETHING_WENT_WRONG = "Something went wrong. Please try again!"
     }
 }
--- app/src/test/java/com/veselovvv/drinks/presentation/cocktails/BaseCocktailsDomainToUiMapperTest.kt
+++ app/src/test/java/com/veselovvv/drinks/presentation/cocktails/BaseCocktailsDomainToUiMapperTest.kt
@@ -1,51 +1,41 @@
 package com.veselovvv.drinks.presentation.cocktails
 
-import com.veselovvv.drinks.R
+import com.veselovvv.drinks.TestResourceProvider
 import com.veselovvv.drinks.core.ErrorType
-import com.veselovvv.drinks.core.ResourceProvider
 import com.veselovvv.drinks.domain.cocktails.CocktailDomain
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class BaseCocktailsDomainToUiMapperTest {
     private val cocktailDomainToUiMapper = BaseCocktailDomainToUiMapper()
-    private val cocktailsDomainToUiMapper = BaseCocktailsDomainToUiMapper(
+    private val mapper = BaseCocktailsDomainToUiMapper(
         TestResourceProvider(), cocktailDomainToUiMapper
     )
 
     @Test
     fun test_success() {
-        val data = listOf(
+        val cocktails = listOf(
             CocktailDomain("1", "Margarita", "Ordinal", "https://somephotopath1"),
             CocktailDomain("12", "Martini", "Ordinal", "https://somephotopath2")
         )
-        val expected = CocktailsUi.Success(data, cocktailDomainToUiMapper)
-        val actual = cocktailsDomainToUiMapper.map(data)
+        val expected = CocktailsUi.Success(cocktails, cocktailDomainToUiMapper)
+        val actual = mapper.map(cocktails)
         assertEquals(expected, actual)
     }
 
     @Test
     fun test_fail() {
         var expected = CocktailsUi.Fail(NO_CONNECTION_MESSAGE)
-        var actual = cocktailsDomainToUiMapper.map(ErrorType.NO_CONNECTION)
+        var actual = mapper.map(ErrorType.NO_CONNECTION)
         assertEquals(expected, actual)
 
         expected = CocktailsUi.Fail(SERVICE_UNAVAILABLE_MESSAGE)
-        actual = cocktailsDomainToUiMapper.map(ErrorType.SERVICE_UNAVAILABLE)
+        actual = mapper.map(ErrorType.SERVICE_UNAVAILABLE)
         assertEquals(expected, actual)
-    }
-
-    class TestResourceProvider : ResourceProvider {
-        override fun getString(id: Int) = when (id) {
-            R.string.no_connection_message -> NO_CONNECTION_MESSAGE
-            R.string.service_unavailable_message -> SERVICE_UNAVAILABLE_MESSAGE
-            else -> SOMETHING_WENT_WRONG
-        }
     }
 
     companion object {
         private const val NO_CONNECTION_MESSAGE = "No connection. Please try again!"
         private const val SERVICE_UNAVAILABLE_MESSAGE = "Service unavailable. Please try again!"
-        private const val SOMETHING_WENT_WRONG = "Something went wrong. Please try again!"
     }
 }
