b9cbb0b46c3be848eea10421c78bb3b040b237f6
veselovvv <vycheslav5veselov@gmail.com>
2022-08-04 18:16:07 +0300

    Added logic for CategoriesFragment

--- app/src/main/java/com/veselovvv/drinks/CategoriesFragment.kt
+++ app/src/main/java/com/veselovvv/drinks/CategoriesFragment.kt
@@ -1,11 +1,41 @@
 package com.veselovvv.drinks
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.veselovvv.drinks.databinding.FragmentCategoriesBinding
 import com.veselovvv.drinks.presentation.core.BaseFragment
 
 class CategoriesFragment : BaseFragment<FragmentCategoriesBinding>() {
     override fun setupViewBinding(inflater: LayoutInflater, container: ViewGroup?) =
         FragmentCategoriesBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.categoriesCategoriesCardView.setOnClickListener {
+            navigateToFragmentWithCategory("c")
+        }
+
+        binding.categoriesGlassCardView.setOnClickListener {
+            navigateToFragmentWithCategory("g")
+        }
+
+        binding.categoriesIngredientsCardView.setOnClickListener {
+            navigateToFragmentWithCategory("i")
+        }
+
+        binding.categoriesAlcoholCardView.setOnClickListener {
+            navigateToFragmentWithCategory("a")
+        }
+    }
+
+    private fun navigateToFragmentWithCategory(categoryKey: String) {
+        val bundle = Bundle()
+        bundle.putString("category", categoryKey)
+        requireActivity().findNavController(R.id.fragment_container_view)
+            .navigate(R.id.subcategoriesFragment, bundle)
+    }
 }
