c51de8fa3e532f9fd6b84e7ebaa17b8488d41244
veselovvv <vycheslav5veselov@gmail.com>
2022-08-30 19:32:01 +0300

    Added unit tests of subcategory cocktails for presentation layer

---
+++ app/src/test/java/com/veselovvv/drinks/presentation/subcategorycocktails/BaseSubcategoryCocktailsDomainToUiMapperTest.kt
@@ -0,0 +1,40 @@
+package com.veselovvv.drinks.presentation.subcategorycocktails
+
+import com.veselovvv.drinks.TestResourceProvider
+import com.veselovvv.drinks.core.ErrorType
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class BaseSubcategoryCocktailsDomainToUiMapperTest {
+    private val subcategoryCocktailDomainToUiMapper = BaseSubcategoryCocktailDomainToUiMapper()
+    private val mapper = BaseSubcategoryCocktailsDomainToUiMapper(
+        TestResourceProvider(), subcategoryCocktailDomainToUiMapper
+    )
+
+    @Test
+    fun test_success() {
+        val cocktails = listOf(
+            SubcategoryCocktailDomain("1", "Margarita", "https://somephotopath1"),
+            SubcategoryCocktailDomain("12", "Martini", "https://somephotopath2")
+        )
+        val expected = SubcategoryCocktailsUi.Success(cocktails, subcategoryCocktailDomainToUiMapper)
+        val actual = mapper.map(cocktails)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail() {
+        var expected = SubcategoryCocktailsUi.Fail(NO_CONNECTION_MESSAGE)
+        var actual = mapper.map(ErrorType.NO_CONNECTION)
+        assertEquals(expected, actual)
+
+        expected = SubcategoryCocktailsUi.Fail(SERVICE_UNAVAILABLE_MESSAGE)
+        actual = mapper.map(ErrorType.SERVICE_UNAVAILABLE)
+        assertEquals(expected, actual)
+    }
+
+    companion object {
+        private const val NO_CONNECTION_MESSAGE = "No connection. Please try again!"
+        private const val SERVICE_UNAVAILABLE_MESSAGE = "Service unavailable. Please try again!"
+    }
+}
