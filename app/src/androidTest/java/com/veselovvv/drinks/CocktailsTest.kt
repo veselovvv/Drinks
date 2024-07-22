54f29e73b6d0bb34faa9baa6cf1fb25ec2e0353a
veselovvv <vycheslav5veselov@gmail.com>
2024-07-22 12:48:53 +0300

    Added load cocktail details and go back ui test

--- app/src/androidTest/java/com/veselovvv/drinks/CocktailsTest.kt
+++ app/src/androidTest/java/com/veselovvv/drinks/CocktailsTest.kt
@@ -1,5 +1,6 @@
 package com.veselovvv.drinks
 
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -145,5 +146,132 @@ class CocktailsTest {
                 Pair("Clover Club", "Ordinary Drink")
             )
         )
+    }
+
+    /**
+     * Check Cocktails Page is visible
+     * Check cocktails list state
+     * 1. Click on first item in list (index = 0)
+     * Check Cocktail Details Page is visible
+     * Check cocktail details state
+     * 2. Recreate activity
+     * Check Cocktail Details Page is visible
+     * Check cocktail details state
+     * 3. Swipe to refresh
+     * Check error state with text "No connection. Please try again!"
+     * 4. Click "Try again" button
+     * Check Cocktail Details Page is visible
+     * Check cocktail details state
+     * 5. Recreate activity
+     * Check Cocktail Details Page is visible
+     * Check cocktail details state
+     * 6. Press back button
+     * Check Cocktail Details Page is not visible
+     * Check Cocktails Page is visible
+     * Check cocktails list state
+     * 7. Recreate activity
+     * Check Cocktail Details Page is not visible
+     * Check Cocktails Page is visible
+     * Check cocktails list state
+     */
+    @Test
+    fun loadCocktailDetailsAndGoBack() {
+        val cocktailsPage = CocktailsPage()
+
+        with(cocktailsPage) {
+            checkIsVisible()
+            checkCocktailsListState(
+                cocktails = listOf(
+                    Pair("Margarita", "Ordinal"),
+                    Pair("Martini", "Ordinal"),
+                    Pair("Clover Club", "Ordinary Drink")
+                )
+            )
+            clickOnItemInList(index = 0)
+        }
+
+        val cocktailDetailsPage = CocktailDetailsPage()
+
+        with(cocktailDetailsPage) {
+            checkIsVisible()
+            checkCocktailDetailsState(
+                name = "Margarita",
+                category = "Ordinal",
+                alcoholic = "Alcoholic",
+                glass = "Cocktail glass",
+                instructions = "Rub the rim of the glass with the lime slice to make the salt stick to it.",
+                ingredient1 = "Tequila",
+                ingredient2 = "Triple sec",
+                ingredient3 = "Lime juice",
+                ingredient4 = "Salt"
+            )
+            activityScenarioRule.scenario.recreate()
+            checkIsVisible()
+            checkCocktailDetailsState(
+                name = "Margarita",
+                category = "Ordinal",
+                alcoholic = "Alcoholic",
+                glass = "Cocktail glass",
+                instructions = "Rub the rim of the glass with the lime slice to make the salt stick to it.",
+                ingredient1 = "Tequila",
+                ingredient2 = "Triple sec",
+                ingredient3 = "Lime juice",
+                ingredient4 = "Salt"
+            )
+            swipeToRefresh()
+            checkErrorState(message = "No connection. Please try again!")
+            clickTryAgainButton()
+            checkIsVisible()
+            checkCocktailDetailsState(
+                name = "Margarita",
+                category = "Ordinal",
+                alcoholic = "Alcoholic",
+                glass = "Cocktail glass",
+                instructions = "Rub the rim of the glass with the lime slice to make the salt stick to it.",
+                ingredient1 = "Tequila",
+                ingredient2 = "Triple sec",
+                ingredient3 = "Lime juice",
+                ingredient4 = "Salt"
+            )
+            activityScenarioRule.scenario.recreate()
+            checkIsVisible()
+            checkCocktailDetailsState(
+                name = "Margarita",
+                category = "Ordinal",
+                alcoholic = "Alcoholic",
+                glass = "Cocktail glass",
+                instructions = "Rub the rim of the glass with the lime slice to make the salt stick to it.",
+                ingredient1 = "Tequila",
+                ingredient2 = "Triple sec",
+                ingredient3 = "Lime juice",
+                ingredient4 = "Salt"
+            )
+        }
+
+        pressBack()
+
+        cocktailDetailsPage.checkIsNotVisible()
+        with(cocktailsPage) {
+            checkIsVisible()
+            checkCocktailsListState(
+                cocktails = listOf(
+                    Pair("Margarita", "Ordinal"),
+                    Pair("Martini", "Ordinal"),
+                    Pair("Clover Club", "Ordinary Drink")
+                )
+            )
+        }
+        activityScenarioRule.scenario.recreate()
+        cocktailDetailsPage.checkIsNotVisible()
+        with(cocktailsPage) {
+            checkIsVisible()
+            checkCocktailsListState(
+                cocktails = listOf(
+                    Pair("Margarita", "Ordinal"),
+                    Pair("Martini", "Ordinal"),
+                    Pair("Clover Club", "Ordinary Drink")
+                )
+            )
+        }
     }
 }
