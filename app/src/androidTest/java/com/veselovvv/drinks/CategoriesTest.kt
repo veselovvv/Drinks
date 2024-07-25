8435ecdabd0807c773b8b8ecf00ae33eb7f910f6
veselovvv <vycheslav5veselov@gmail.com>
2024-07-25 09:09:05 +0300

    Added load glass subcategories and open subcategory and go back ui test

--- app/src/androidTest/java/com/veselovvv/drinks/CategoriesTest.kt
+++ app/src/androidTest/java/com/veselovvv/drinks/CategoriesTest.kt
@@ -402,4 +402,76 @@ class CategoriesTest {
         subcategoryCocktailsPage.checkIsNotVisible()
         subcategoriesPage.checkIsVisible()
     }
+
+    /**
+     * Check Cocktails Page is visible
+     * 1. Click on "Categories" tab in BottomNavigation
+     * Check Categories Page is visible
+     * Check categories state
+     * 2. Click on "Glass" CardView
+     * Check Subcategories Page is visible
+     * Check subcategories list state
+     * 3. Click on first item in list (index = 0)
+     * Check Subcategory Cocktails Page is visible
+     * Check subcategory cocktails list state
+     * 4. Swipe to refresh
+     * Check error state with text "No connection. Please try again!"
+     * 5. Click "Try again" button
+     * Check Subcategory Cocktails Page is visible
+     * Check subcategory cocktails list state
+     * 6. Press back button
+     * Check Subcategory Cocktails Page is not visible
+     * Check Subcategories Page is visible
+     */
+    @Test
+    fun loadGlassSubcategoriesAndOpenSubcategoryAndGoBack() {
+        with(CocktailsPage()) {
+            checkIsVisible()
+            clickOnCategoriesTab()
+        }
+
+        val categoriesPage = CategoriesPage()
+
+        with(categoriesPage) {
+            checkIsVisible()
+            checkCategoriesState(
+                category1 = "Categories",
+                category2 = "Glass",
+                category3 = "Ingredients",
+                category4 = "Alcohol"
+            )
+            clickOnGlassCardView()
+        }
+
+        val subcategoriesPage = SubcategoriesPage()
+
+        with(subcategoriesPage) {
+            checkIsVisible()
+            checkSubcategoriesListState(
+                subcategories = listOf("Cocktail glass", "Whiskey glass", "Wine glass")
+            )
+            clickOnItemInList(index = 0)
+        }
+
+        val subcategoryCocktailsPage = SubcategoryCocktailsPage()
+
+        with(subcategoryCocktailsPage) {
+            checkIsVisible()
+            checkSubcategoryCocktailsListState(
+                subcategoryCocktails = listOf("A. J.", "Adam", "Almeria")
+            )
+            swipeToRefresh()
+            checkErrorState(message = "No connection. Please try again!")
+            clickTryAgainButton()
+            checkIsVisible()
+            checkSubcategoryCocktailsListState(
+                subcategoryCocktails = listOf("A. J.", "Adam", "Almeria")
+            )
+        }
+
+        pressBack()
+
+        subcategoryCocktailsPage.checkIsNotVisible()
+        subcategoriesPage.checkIsVisible()
+    }
 }
