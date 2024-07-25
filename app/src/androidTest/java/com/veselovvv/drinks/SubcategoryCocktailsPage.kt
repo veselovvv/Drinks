c3500151868d9c5f697f4b1a8d209f0bfde772f8
veselovvv <vycheslav5veselov@gmail.com>
2024-07-25 08:43:44 +0300

    Added SubcategoryCocktailsPage

---
+++ app/src/androidTest/java/com/veselovvv/drinks/SubcategoryCocktailsPage.kt
@@ -0,0 +1,16 @@
+package com.veselovvv.drinks
+
+class SubcategoryCocktailsPage : AbstractPage(R.id.subcategory_cocktails_root_layout) {
+    private val recyclerViewUi = SubcategoryCocktailsRecyclerViewUi()
+    private val swipeToRefreshUi = SwipeToRefreshUi(
+        rootLayout,
+        R.id.subcategory_cocktails_swipe_to_refresh
+    )
+
+    fun checkSubcategoryCocktailsListState(subcategoryCocktails: List<String>) =
+        recyclerViewUi.checkSubcategoryCocktailsListState(subcategoryCocktails = subcategoryCocktails)
+
+    fun swipeToRefresh() = swipeToRefreshUi.swipeToRefresh()
+    fun checkErrorState(message: String) = recyclerViewUi.checkErrorState(message = message)
+    fun clickTryAgainButton() = recyclerViewUi.clickTryAgainButton()
+}
